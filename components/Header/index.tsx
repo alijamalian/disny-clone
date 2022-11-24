098d8782b3ebe68bacd1692a6d551aea042802d6
alijamalian <alijamalian@gmail.com>
2022-11-24 07:42:34 +0100

    first commit

---
+++ components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Nav } from './styles'
+
+type Props = {}
+
+function Header() {
+  return (
+    <Nav>Header</Nav>
+  )
+}
+
+export default Header
---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider, DefaultTheme } from 'styled-components'
+import Header from '../components/Header'
+import GlobalStyle from '../globalstyles'
+
+const theme: DefaultTheme = {
+  
+  primary: "#040714",
+  secondary: "#0070f3",
+  white: "#fff",
+  brightBlue: "#3E64E5",
+  silver: "silver",
+  darkGrey: "#31343e",
+
+}
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Header/>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
